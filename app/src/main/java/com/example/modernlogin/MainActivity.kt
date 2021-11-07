b098d73bebefa8e220124017e3707231684c85cb
= <=>
2021-11-07 21:07:49 +0300

    Login_layout refactor

--- app/src/main/java/com/example/modernlogin/MainActivity.kt
+++ app/src/main/java/com/example/modernlogin/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.modernlogin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_login)
+
+
     }
+
 }
--- app/src/main/java/com/example/modernlogin/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/modernlogin/ui/login/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.example.modernlogin.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.modernlogin.R
+import com.example.modernlogin.ui.home.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private var textView2 : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        textView2 = findViewById(R.id.textView2)
+
     }
 }
