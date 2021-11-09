ab9f3d043abbd4435e266308127e84f0507fb4ea
= <=>
2021-11-09 16:32:24 +0300

    finito

--- app/src/main/java/com/example/modernlogin/ui/home/HomeActivity.kt
+++ app/src/main/java/com/example/modernlogin/ui/home/HomeActivity.kt
@@ -1,12 +1,26 @@
 package com.example.modernlogin.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.example.modernlogin.R
+import androidx.appcompat.app.AppCompatActivity
+import com.example.modernlogin.databinding.ActivityHomeBinding
+import com.example.modernlogin.ui.login.LoginActivity
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnStarted.setOnClickListener {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
