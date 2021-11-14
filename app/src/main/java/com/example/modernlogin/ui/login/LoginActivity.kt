c811a2e3277fa43f1c10f8f86e79939f9982d0a8
= <=>
2021-11-14 20:29:42 +0300

    login and home ldpi-xhdpi refactor

--- app/src/main/java/com/example/modernlogin/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/modernlogin/ui/login/LoginActivity.kt
@@ -3,8 +3,6 @@ package com.example.modernlogin.ui.login
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.modernlogin.R
-import com.example.modernlogin.databinding.ActivityHomeBinding
 import com.example.modernlogin.databinding.ActivityLoginBinding
 import com.example.modernlogin.ui.register.RegisterActivity
 
